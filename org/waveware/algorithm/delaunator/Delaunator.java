22551fd45046af68cb8a90ed5eb3171e54d2ee28
waveware4ai <50553398+waveware4ai@users.noreply.github.com>
2021-04-19 21:15:36 +0900

    Add files via upload

--- org/waveware/algorithm/delaunator/Delaunator.java
+++ org/waveware/algorithm/delaunator/Delaunator.java
@@ -23,8 +23,8 @@ import javax.imageio.ImageIO;
 This code is java port of delaunator.
 Delaunator triangulation algorithm is incredibly fast and robust library for point of 2D clouds.
 I modified it by referring to the code c++, c# version.
-to verify contents, go to original site https://github.com/mapbox/delaunator
- */
+For more information, go to original site https://github.com/mapbox/delaunator
+*/
 
 public class Delaunator
 {
